0b5086f0aefb751eaedf34559bea52c897d4d4ce
Charles Lu <kuanminglu0529@gmail.com>
2021-04-30 01:59:24 -0700

    structure for filter component started

---
+++ client/src/components/Filters/Filters.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './filters.module.scss';
+
+function Filters(props) {
+    return (
+        <div className={classes.filter}>
+            <div className={classes.filter__pageTitle}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Filters
