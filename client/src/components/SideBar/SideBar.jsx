eb4b0f4551a9baf7365b43b4b639d1b6e45c9897
Peter Reyes <codezilla@Peters-MBP.attlocal.net>
2021-04-30 11:47:51 -0500

    added new style to sidebar

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import xIcon from "../../assets/icons/x.svg";
+import logoImg from '../../assets/images/shieldLogo.svg';
 import "./sideBar.scss";
+import home from '../../assets/icons/home.svg';
+import profile from '../../assets/icons/profile.svg';
+import filters from '../../assets/icons/filters.svg';
+import settings from '../../assets/icons/settings.svg';
 
 export default function SideBar(props) {
     return (
@@ -12,29 +17,30 @@ export default function SideBar(props) {
             <section className={`sidebar  ${props.showNav}`}>
                 <div className="sidebar__top-section">
                     <div className="sidebar__img-wrap">
-                        <img
+                        <img 
                             className="sidebar__img"
                             src={xIcon}
                             alt="X button to close page"
                             onClick={(event) => props.handleCancel(event)}
                         />
                     </div>
+                    <img className="sidebar__logo" src={logoImg} alt="Shield Logo"/>
                 </div>
                 <div className="sidebar__bottom-section">
                     <div className="sidebar__selection-wrap">
-                        <div className="sidebar__checkbox"></div>
+                        <img className="sidebar__checkbox" src={home} alt="select home icon"/>
                         <h2 className="sidebar__select-category">Home</h2>
                     </div>
                     <div className="sidebar__selection-wrap">
-                        <div className="sidebar__checkbox"></div>
+                        <img className="sidebar__checkbox" src={profile} alt="select profile icon"/>
                         <h2 className="sidebar__select-category">Profile</h2>
                     </div>
                     <div className="sidebar__selection-wrap">
-                        <div className="sidebar__checkbox"></div>
+                        <img className="sidebar__checkbox" src={filters} alt="select filters icon"/>
                         <h2 className="sidebar__select-category">Filters</h2>
                     </div>
                     <div className="sidebar__selection-wrap">
-                        <div className="sidebar__checkbox"></div>
+                        <img className="sidebar__checkbox" src={settings} alt="select settings icon"/>
                         <h2 className="sidebar__select-category">Settings</h2>
                     </div>
                     <h2 className="sidebar__contact-us">
